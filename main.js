73a0773a02a31143f0c9c6bd7c63706a414b4f47
夕小汐 <885204655@qq.com>
2022-12-12 23:13:13 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 import App from './App'
 
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+
+$http.beforeRequest = function() {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装消息提示
+uni.$showMsg = function(title = '数据加载失败', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
   return {
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,122 @@
 <template>
   <view>
-    
+    <!-- 轮播图 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <navigator :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id" class="swiper-item"><image :src="item.image_src"></image></navigator>
+      </swiper-item>
+    </swiper>
+    <!-- 分类导航 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)"><image :src="item.image_src" class="nav-img"></image></view>
+    </view>
+    <!-- 楼层结构 -->
+    <view class="floor-list">
+      <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <view class="floor-img-box">
+          <navigator :url="item.product_list[0].url" class="left-img-box">
+            <image :src="item.product_list[0].image_src" :style="{ width: item.product_list[0].image_width + 'rpx' }" mode="widthFix"></image>
+          </navigator>
+          <view class="right-img-box">
+            <navigator :url="item2.url" class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0">
+              <image :src="item2.image_src" mode="widthFix" :style="{ width: item2.image_width + 'rpx' }"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-      };
+export default {
+  data() {
+    return {
+      swiperList: [],
+      // 分类导航
+      navList: [],
+      // 楼层数据
+      floorList: []
+    };
+  },
+  onLoad() {
+    this.getSwiperList();
+    this.getNavList();
+    this.getFloorList();
+  },
+  methods: {
+    async getSwiperList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata');
+      // console.log(res);
+      if (res.meta.status !== 200) {
+        return uni.$showMsg();
+      }
+      this.swiperList = res.message;
+    },
+    async getNavList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/catitems');
+      if (res.meta.status !== 200) return uni.$showMsg();
+      this.navList = res.message;
+    },
+    navClickHandler(item) {
+      if ((item.name = '分类')) {
+        uni.switchTab({
+          url: '/pages/cate/cate'
+        });
+      }
+    },
+    async getFloorList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/floordata');
+      if (res.meta.status !== 200) return uni.$showMsg();
+      // 处理url
+      res.message.forEach(f => {
+        f.product_list.forEach(p => {
+          p.url = '/subpkg/goods_list/goods_list?' + p.navigator_url.split('?')[1];
+        });
+      });
+      this.floorList = res.message;
     }
   }
+};
 </script>
 
 <style lang="scss">
-
+swiper {
+  height: 330rpx;
+  .swiper-item,
+  image {
+    width: 100%;
+    height: 100%;
+  }
+}
+.nav-list {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px 0;
+  .nav-item {
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+}
+.floor-list {
+  .floor-item {
+    .floor-title {
+      height: 60rpx;
+      width: 100%;
+      // display: flex;
+    }
+  }
+}
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
+}
 </style>
